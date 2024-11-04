340f93554fa6e2810192b2f0b4f5fd569b3cabf6
Lukas Kofler <lukaskofler@hotmail.de>
2024-11-04 01:48:45 +0000

    just a socket

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
 import "fmt"
+import "net"
+
+var (
+	port = 8050;
+	maxRequestSize = 1<<24;
+	version = "0.0.1"
+)
+
+
 
 func main(){
-	fmt.Println("Hello World!");
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port));
+
+	if err != nil {
+		fmt.Println(err);
+		return;
+	}
+	
+	fmt.Println(fmt.Sprintf("GoTTP v%s listening on port %d", version, port));
+
+	for {
+		conn, err := ln.Accept();
+
+		if err != nil {
+			fmt.Println(err);
+			continue;
+		}
+
+		go handleConnection(conn);
+	}
+}
+
+
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close();
+
+	buf := make([]byte, maxRequestSize);
+	_, err := conn.Read(buf);
+
+	if err != nil {
+		fmt.Println(err);
+		return;
+	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\nServer: GoTTP\nContent-Type: text/html\nContent-Lenght: 6\n\n{Hi!}\n"));
 }
